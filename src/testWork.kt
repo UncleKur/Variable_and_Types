79e29bb546ef2363684f18db8952dcc48586ef72
Михаил Курочкин <eaq19933@gmail.com>
2024-05-23 18:03:39 +0300

    Solved task

--- src/testWork.kt
+++ src/testWork.kt
@@ -1,16 +1,36 @@
 fun main() {
-
+    //замокать - заполнить программу какими-то данными, что бы получить результат работы программы
     val expectedEarnings = 500
 
-    val day1Earnings = 750
-    val day2Earnings = 600
-    val day3Earnings = 200
-    val day4Earnings = -100
-    val day5Earnings = 1000
+    val day1Earnings = calculateEarningPerDay(6, 8)
+    val day2Earnings = calculateEarningPerDay(3, 2)
+    val day3Earnings = calculateEarningPerDay(4, 11)
+    val day4Earnings = calculateEarningPerDay(7, 0)
+    val day5Earnings = calculateEarningPerDay(3, 7)
+    val day6Earnings = calculateEarningPerDay(0, 0)
 
-    val actualEarnings = (day1Earnings + day2Earnings + day3Earnings + day4Earnings + day5Earnings) / 5
+    val actualEarnings = (day1Earnings + day2Earnings + day3Earnings + day4Earnings + day5Earnings + day6Earnings) / 5f
 
     val isPromiseFulfilled = actualEarnings >= expectedEarnings
+
+    println(actualEarnings)
     println(isPromiseFulfilled)
 
 }
+
+fun calculateEarningPerDay(goodSteelWeight: Int, badSteelWeight: Int): Int {
+    val goodSteelPrice = 120
+    val badSteelPrice = 50
+    val gasCostPerTrip = 300
+    return goodSteelWeight * goodSteelPrice + badSteelWeight * badSteelPrice - gasCostPerTrip
+
+}
+
+/*
+хороший металл стоил 110 рублей, а плохой — 55
+Ответ: 514 средний чек, да выполнил обещание
+затраты на бензин были бы 350 рублей, а не 300
+Ответ: 482 ср. чек, нет не выполнил
+на шестой день он поехал бы без жены и застрял в гараже с друзьями, ничего не добыв
+Ответ: 472 ср. чек, нет не выполнил
+**/
